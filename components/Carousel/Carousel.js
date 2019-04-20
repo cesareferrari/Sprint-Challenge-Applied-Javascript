dc91f1e607876a633cf71be826f67f4324ffc8d9
Cesare Ferrari <cesare@maryandferrari.com>
2019-04-20 13:20:24 -0400

    Create carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,25 +1,56 @@
 
 class Carousel {
+  constructor(carouselElement) {
+    this.carouselElement = carouselElement;
+    this.leftButton = this.carouselElement.querySelector('.left-button');
+    this.rightButton = this.carouselElement.querySelector('.right-button');
+    this.images = Array.from(this.carouselElement.querySelectorAll('img'));
+    this.currentIndex = 0;
+    this.images[this.currentIndex].style.display = 'block';
 
+    this.rightButton.addEventListener('click', () => this.clickRight());
+    this.leftButton.addEventListener('click', () => this.clickLeft());
+  }
+
+  clickRight() {
+    this.images.forEach(image => image.style.display = 'none');
+    this.currentIndex++;
+    this.images[this.currentIndex].style.display = 'block';
+  }
+
+  clickLeft() {
+    this.images.forEach(image => image.style.display = 'none');
+    this.currentIndex--;
+    this.images[this.currentIndex].style.display = 'block';
+  }
 }
 
-const carousel = document.querySelector('.carousel');
-const leftButton = carousel.querySelector('.left-button');
-const rightButton = carousel.querySelector('.right-button');
-const imageElements = carousel.querySelectorAll('img');
-const images = Array.from(imageElements);
+const carouselElement = document.querySelector('.carousel');
+const carousel = new Carousel(carouselElement);
 
-let currentIndex = 0;
+console.log(carousel);
 
-leftButton.addEventListener('click', function(event) {
-  images[currentIndex--].style.display = 'block';
-})
 
-rightButton.addEventListener('click', function(event) {
-  images[currentIndex++].style.display = 'block';
-})
 
-console.log(images);
+
+// const images = Array.from(imageElements);
+// 
+// let currentIndex = 0;
+// 
+// leftButton.addEventListener('click', function(event) {
+//   images[currentIndex--].style.display = 'block';
+// })
+// 
+// rightButton.addEventListener('click', function(event) {
+//   images[currentIndex++].style.display = 'block';
+// })
+// 
+// console.log(images);
+// 
+
+
+
+
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
